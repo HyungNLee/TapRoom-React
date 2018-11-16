75056c80235f9957d4df96121f8e877ad5a0de12
Hyung Lee <hyungnaelee@gmail.com>
2018-11-16 10:31:14 -0800

    Add Homepage component and body styling.

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+
+/*
+  import { Link } from 'react-router-dom';
+  <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
+*/
+
+function App(){
+  var styles = {
+  };
+  return (
+    <div style={styles}>
+      <style jsx>{`
+        font-family: Helvetica;
+      `}</style>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
+    </div>
+  );
+}
+
+//App.propTypes = {
+//};
+
+export default App;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <style jsx>{`
+        h1 {
+          color: white;
+          text-align: center;
+        }
+        .jumbotron {
+          background-color: ;
+        }
+      `}</style>
+      <div className="title">
+        <h1>Welcome to the Tap Room</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
